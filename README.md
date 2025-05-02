01ab45e126cee772c0d41e42cd35d5b44e56c2b1
Cariad Eccleston <cariad@cariad.earth>
2025-05-02 10:25:00 +0100

    Stub the project (#1)

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# s3same
-A Rust library for comparing AWS S3 objects
+# s3same: Compare AWS S3 objects in Rust
+
+## Installation
+
+```bash
+cargo add s3same
+```
+
+## Examples
+
+### Check if a local file is of an expected size
+
+`s3uri::from_uri` parses a string URI and returns a `s3uri::S3Uri` struct with the bucket name and key set.
+
+```rust
+use s3same::LocalPath;
+
+let result = std::path::Path::new("LICENSE")
+    .is_size(1073)
+    .unwrap();
+
+assert!(result);
+```
+
+## Author
+
+Hello! 👋 I'm Cariad Eccleston. You can find me at [cariad.earth](https://www.cariad.earth), [github.com/cariad](https://github.com/cariad), [linkedin.com/in/cariad](https://linkedin.com/in/cariad) and [@cariad.earth](https://bsky.app/profile/cariad.earth) on Bluesky.
---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+#[doc = include_str!("../README.md")]
+use std::{fs, io::Error, path::Path};
+
+pub trait LocalPath {
+    fn is_size(&self, size: u64) -> Result<bool, Error>;
+}
+
+impl LocalPath for Path {
+    /// Checks if a local file is of an expected size.
+    ///
+    /// ```rust
+    /// use s3same::LocalPath;
+    ///
+    /// let result = std::path::Path::new("LICENSE")
+    ///     .is_size(1073)
+    ///     .unwrap();
+    ///
+    /// assert!(result);
+    /// ```
+    fn is_size(&self, size: u64) -> Result<bool, std::io::Error> {
+        match fs::metadata(self) {
+            Ok(metadata) => Ok(metadata.len() == size),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::LocalPath;
+
+    #[test]
+    fn is_size_false() {
+        let result = std::path::Path::new("LICENSE").is_size(1072).unwrap();
+        assert!(!result);
+    }
+
+    #[test]
+    fn is_size_true() {
+        let result = std::path::Path::new("LICENSE").is_size(1073).unwrap();
+        assert!(result);
+    }
+}
